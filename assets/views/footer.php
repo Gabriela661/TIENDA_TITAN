a22189f79f2897a41cbe30e0c3f3db2c6658a681
Gabriela661 <rafaelogabriela@gmail.com>
2024-03-07 16:50:50 -0500

    cambios

--- assets/views/footer.php
+++ assets/views/footer.php
@@ -1,65 +1,25 @@
    <!-- Start Footer -->
    <footer class="bg-footer" id="tempaltemo_footer">
-       <div class="container">
-           <div class="row">
-               <div class="col-md-4 pt-5">
-                   <img src="assets/img/logo_titan1.png" style="width: 200px; height:90px"> <br><br>
-                   <ul class="list-unstyled text-light footer-link-list">
-                       <li>
-                           <i class="fas fa-map-marker-alt fa-fw"></i>
-                           123 Consectetur at ligula 10660
-                       </li>
-                       <li>
-                           <i class="fa fa-phone fa-fw"></i>
-                           <a class="text-decoration-none" href="tel:010-020-0340">010-020-0340</a>
-                       </li>
-                       <li>
-                           <i class="fa fa-envelope fa-fw"></i>
-                           <a class="text-decoration-none" href="mailto:info@company.com">titan@company.com</a>
-                       </li>
-                   </ul>
-               </div>
-               <div class="col-md-4 pt-5">
-                   <h2 class="h2 text-light border-bottom pb-3 border-light">Informacion</h2>
-                   <ul class="list-unstyled text-light footer-link-list">
-                       <li><a class="text-decoration-none" href="nosotros.php">Nosotros</a></li>
-                       <li><a class="text-decoration-none" href="tienda.php">Tienda</a></li>
-                       <li><a class="text-decoration-none" href="contacto.php">Contactanos</a></li>
-                   </ul>
-               </div>
-           </div>
-
-       </div>
-
-       <div class="w-100 bg-black py-3">
-           <div class="container">
-               <div class="col-12">
-                   <ul class="list-inline text-left footer-icons d-inline-block">
-                       <li class="list-inline-item border border-light rounded-circle text-center">
-                           <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
-                       </li>
-                       <li class="list-inline-item border border-light rounded-circle text-center">
-                           <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram fa-lg fa-fw"></i></a>
-                       </li>
-                       <li class="list-inline-item border border-light rounded-circle text-center">
-                           <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
-                       </li>
-                       <li class="list-inline-item border border-light rounded-circle text-center">
-                           <a class="text-light text-decoration-none" target="_blank" href="https://www.linkedin.com/"><i class="fab fa-linkedin fa-lg fa-fw"></i></a>
-                       </li>
-                   </ul>
-               </div>
-               <div class="row pt-1">
-                   <div class="col-12">
-                       <p class="text-center text-light">
-                           Copyright &copy; 2024 Company Name
-                           | <a rel="sponsored" href="https://templatemo.com" target="_blank">Titan tienda</a>
-                       </p>
-                   </div>
-               </div>
-           </div>
-       </div>
-
-       </div>
-
+       
+   <div class="container-fluid bg-dark text-light has-height-md middle-items border-top text-center wow fadeIn">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>EMAIL US</h3>
+                <P class="text-muted">info@website.com</P>
+            </div>
+            <div class="col-sm-4">
+                <h3>CALL US</h3>
+                <P class="text-muted">(123) 456-7890</P>
+            </div>
+            <div class="col-sm-4">
+                <h3>FIND US</h3>
+                <P class="text-muted">12345 Fake ST NoWhere AB Country</P>
+            </div>
+        </div>
+    </div>
+    <div class="bg-dark text-light text-center border-top wow fadeIn">
+        <p class="mb-0 py-3 text-muted small">&copy; Copyright <script>
+                document.write(new Date().getFullYear())
+            </script> Made with <i class="ti-heart text-danger"></i> By <a href="http://devcrud.com">DevCRUD</a></p>
+    </div>  
    </footer>

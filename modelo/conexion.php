e461ed9ba44e8e11ddffc1cb2591ef13ad28cd80
JHONATHANESPINOZA <JHONATHANESPINOZAORTEGA@GMAIL.COM>
2024-03-08 09:38:12 -0500

    conexion actualizad

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,9 +1,9 @@
 <?php
-class Conexion
+class conexion
 {
     private $servidor = "localhost";
-    private $db = "titan_db";
-    private $charset = "utf8mb4";
+    private $db = "empresa_venta";
+    private $chartset = "utf8mb4";
     private $usuario = "root";
     private $password = "";
     public $pdo = null;
@@ -11,25 +11,6 @@ class Conexion
 
     function __construct()
     {
-        try {
-            $this->pdo = new PDO("mysql:host={$this->servidor};dbname={$this->db};charset={$this->charset}", $this->usuario, $this->password, $this->atributos);
-        } catch (PDOException $e) {
-            // Manejar la excepción aquí
-            echo "Error de conexión: " . $e->getMessage();
-            // Opcionalmente, puedes lanzar la excepción nuevamente para que sea manejada por el código que llama a la instancia de la clase Conexion
-            // throw $e;
-        }
+        $this->pdo = new PDO("mysql:host={$this->servidor};dbname={$this->db};chartset={$this->chartset}", $this->usuario, $this->password, $this->atributos);
     }
-}
-
-try {
-    $conexion = new Conexion();
-    if ($conexion->pdo) {
-        echo "Conexión correcta: Se estableció una conexión PDO.";
-    } else {
-        echo "Error al conectar con la base de datos.";
-    }
-} catch (PDOException $e) {
-    // Si la conexión falla, puedes manejar la excepción aquí
-    echo "Error al conectar con la base de datos: " . $e->getMessage();
 }
--- vista/usuarios.php
+++ vista/usuarios.php
@@ -2,83 +2,130 @@
 <?php
 include_once "assets/views/nav.php";
 ?>
- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
- 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Theme style -->
-    <!-- <link rel="stylesheet" href="../../dist/css/adminlte.min.css"> -->
-    <link rel="stylesheet" href="../vista/assets/css/adminlte.min.css">
-    <!-- <link rel="stylesheet" href="assets/css/stilos.css"> -->
-    <link rel="stylesheet" href="../vista/assets/css/stilos.css">
 
 <body>
-<div class="wrapper">
-    <!-- Modal -->
+  <div class="wrapper">
+
+    <!-- inicio Modal  crear usuario-->
     <div class="modal fade" id="crearUsuario" tabindex="-1" role="dialog" aria-labelledby="#crearUsuario" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="#crearUsuario">Registrar Usuario</h5>
+            <h5 class="modal-title" id="#crearUsuario">Registrar Nuevo Usuario</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
-
-            <form id="datos_usuario" method="POST">
+            <form id="form_usuario" method="POST">
               <div class="card-body">
-
                 <div class="form-group">
-                  <label for="nombres">Nombres</label>
-                  <input type="text" class="form-control" id="" name="" placeholder="Nombre del usuario">
+                  <label for="nombre_usuario">Nombre</label>
+                  <input type="text" class="form-control" id="nombre_usuario" name="nombre_usuario" placeholder="Nombres" required>
                 </div>
-
                 <div class="form-group">
-                  <label for="apellidos">Apellidos</label>
-                  <input type="text" class="form-control" id="" name="" placeholder="Apellidos del usuario"></input>
+                  <label for="apellidos_usuario">Apellidos</label>
+                  <input type="text" class="form-control" id="apellido_usuario" name="apellido_usuario" placeholder="Apellidos" required>
                 </div>
-
                 <div class="form-group">
-                  <label for="dni">DNI: </label>
-                  <input type="text" class="form-control" id="" name="" placeholder="DNI del usuario">
+                  <label for="dni_usuario">Documento Nacional de Identidad</label>
+                  <input type="text" class="form-control" id="dni_usuario" name="dni_usuario" placeholder="DNI" required maxlength="8">
                 </div>
-
                 <div class="form-group">
-                  <label for="direccion">Direccion: </label>
-                  <input type="text" class="form-control" id="" name="" placeholder="Direccion del usuario">
+                  <label for="telefono_usuario">Número de teléfono</label>
+                  <input type="text" class="form-control" id="telefono_usuario" name="telefono_usuario" placeholder="Número de teléfono" required maxlength="9">
                 </div>
-
                 <div class="form-group">
-                  <label for="telefono">Telefono: </label>
-                  <input type="number" class="form-control" id="" name="" placeholder="N° telefono"></input>
+                  <label for="correo_electronico_usuario">Correo Electrónico</label>
+                  <input type="email" class="form-control" id="correo_electronico_usuario" name="correo_electronico_usuario" placeholder="Correo Electrónico" required>
                 </div>
-
                 <div class="form-group">
-                  <label for="imagen_usuario">Imagen Usuario: </label>
+                  <label for="password_usuario">Contraseña</label>
+                  <input type="text" class="form-control" id="password_usuario" name="password_usuario" placeholder="Contraseña" required>
+                </div>
+                <div class="form-group">
+                  <label for="direccion_usuario">Direccion</label>
+                  <input type="text" class="form-control" id="direccion_usuario" name="direccion_usuario" placeholder="Direccion" required>
+                </div>
+                <div class="form-group">
+                  <label for="foto_usuario">Fotografia del Usuario</label>
                   <br>
-                  <input type="file" name="" id="">
-
+                  <input type="file" name="foto_usuario" id="foto_usuario" required>
                   <center>
-                    <div id="" style="margin-top: 10px;" class="img-thumbnail"></div>
+                    <div id="imagen_preview" style="margin-top: 10px;" class="img-thumbnail"></div>
                   </center>
                 </div>
-
-
+                <div class="form-group">
+                  <label for="rol">Tipo de Usuario</label>
+                  <select type="number" class="form-control" id="rol_usuario" name="rol_usuario" placeholder="Tipo Usuario">
+                  </select>
+                </div>
               </div>
-              <!-- /.card-body -->
-
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-            <button type="submit" class="btn btn-primary">Guardar</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-times-circle"></i> Cancelar</button>
+            <button type="submit" class="btn btn-success"><i class="fas fa-save"></i> Guardar</button>
           </div>
         </div>
       </div>
     </div>
     </form>
+    <!--final  Modal  crear usuario-->
+
+    <!-- inicio Modal  editar usuario-->
+    <div class="modal fade" id="editar_usuario" tabindex="-1" role="dialog" aria-labelledby="#editar_usuario" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Editar Usuario</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form id="form_usuario_editar" method="POST">
+              <div class="card-body">
+                <div class=" d-none form-group">
+                  <input type="text" class="form-control" id="id_usuarioe">
+                </div>
+                <div class="form-group">
+                  <label for="nombre_usuario">Nombres</label>
+                  <input type="text" class="form-control" id="nombre_usuarioe" name="nombre_usuarioe" placeholder="Nombres">
+                </div>
+                <div class="form-group">
+                  <label for="apellidos_usuario">Apellidos</label>
+                  <input type="text" class="form-control" id="apellido_usuarioe" name="apellido_usuarioe" placeholder="Apellidos">
+                </div>
+                <div class="form-group">
+                  <label for="dni_usuario">Documento Nacional de Identidad</label>
+                  <input type="number" class="form-control" id="dni_usuarioe" name="dni_usuarioe" placeholder="DNI">
+                </div>
+                <div class="form-group">
+                  <label for="telefono_usuario"> Numero de telefono </label>
+                  <input type="number" class="form-control" id="telefono_usuarioe" name="telefono_usuarioe" placeholder="Numero de telefono">
+                </div>
+                <div class="form-group">
+                  <label for="correo_electronico_usuario">Correo Electronico</label>
+                  <input type="text" class="form-control" id="correo_electronico_usuarioe" name="correo_electronico_usuarioe" placeholder="Correo Electronico">
+                </div>
+                <div class="form-group">
+                  <label for="direccion_usuario">Direccion</label>
+                  <input type="text" class="form-control" id="direccion_usuarioe" name="direccion_usuarioe" placeholder="Direccion">
+                </div>
+              </div>
+              <!-- /.card-body -->
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-times-circle"></i> Cancelar</button>
+            <button type="submit" class="btn btn-warning"><i class="fas fa-save"></i> Guardar cambios</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </form>
+    <!--final  Modal  editar usuario-->
+
 
 
     <!-- Content Wrapper. Contains page content -->
@@ -88,9 +135,9 @@ include_once "assets/views/nav.php";
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-            <h4><i class="fas fa-users"></i> <b>Lista de Usuarios</b></h4> 
-            <button type="button" class="btn btn-info ml-3" data-toggle="modal" data-target="#crearUsuario">
-                  Nuevo Usuario</button></h1>
+              <h4><i class="fas fa-users"></i> <b>Lista de Usuarios</b></h4>
+              <button type="button" class="btn btn-info ml-3" data-toggle="modal" data-target="#crearUsuario">
+                Nuevo Usuario</button></h1>
             </div>
           </div>
         </div><!-- /.container-fluid -->
@@ -98,71 +145,85 @@ include_once "assets/views/nav.php";
 
       <!-- Main content -->
       <section class="content">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <div class="card">
-                  <div class="card-header">
-                    <h3 class="card-title">Listado de Usuarios</h3>
-                  </div>
-                  <!-- /.card-header -->
-                  <div class="card-body">
-                    <table id="" class="table table-bordered table-striped">
-                      <thead>
-                        <tr>
-                          <th>N°</th>
-                          <th>NOMBRES</th>
-                          <th>APELLIDOS</th>
-                          <th>DNI </th>
-                          <th>DIRECCION </th>
-                          <th>TELEFONO</th>
-                          <th>IMAGEN</th>
-                          <th>ACCIONES</th>
-                        </tr>
-                      </thead>
-                      <tbody id="usuarios_lista">
-                             <?php
-                                $contador = 0;
-                                ?>
-                           <tr>
-                            <td><center><?php echo $contador = $contador + 1;?></center></td>
-                            <td>DOMINGUEZ</td>
-                            <td>VILLANUEVA LINO</td>
-                            <td>747474747</td>
-                            <td>HUAN<i class="mdi mdi-bus-stop-uncovered:"></i></td> 
-                            <td>939939393</td>
-                            <td><img src="assets/img/producto1.jpg" width="50px" alt="asdf"></td>
-                            <td> 
-                              <center>
-                              <div class="btn-group">
-                                   <a href="" type="button" class="btn btn-info"><i class="fa fa-eye"></i></a>&nbsp;  &nbsp;
-                                   <a href="" type="button" class="btn btn-success"><i class="fa fa-pencil-alt"></i> </a>&nbsp;  &nbsp;
-                                   <a href="" type="button" class="btn btn-danger"><i class="fa fa-trash"></i> </a>
-                              </div>                       
-                              </center>
-                            </td>
-                           
-                           </tr>
-
-
-
-                      </tbody>
-                    </table>
-                  </div>
-                  <!-- /.card-body -->
-                </div>
-                <!-- /.card -->
-              </div>
-              <!-- /.col -->
+        <!-- contenedor de usuarios -->
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header p-2">
+              <!-- botones para cambiar entre personal y clientes -->
+              <ul class="nav nav-pills">
+                <li class="nav-item"><a class="nav-link active" href="#personal" data-toggle="tab">Personal</a></li>
+                <li class="nav-item"><a class="nav-link" href="#cliente" data-toggle="tab">Clientes</a></li>
+              </ul>
             </div>
-            <!-- /.row -->
+            <div class="card-body">
+              <div class="tab-content">
+                <!-- Tabla del personal -->
+                <div class="active tab-pane btn-personal" id="personal">
+                  <div class="timeline timeline-inverse">
+                    <div class="card">
+                      <div class="card-header">Personal</div>
+                      <div class="card-body table-responsive">
+                        <table id="example1" class="table table-bordered table-striped">
+                          <thead>
+                            <tr>
+                              <th>N°</th>
+                              <th>Nombres Personal</th>
+                              <th>Apellidos</th>
+                              <th>DNI</th>
+                              <th>Telefono</th>
+                              <th>Correo Electronico</th>
+                              <th>Direccion</th>
+                              <th>Foto</th>
+                              <th>Editar</th>
+                              <th>Eliminar</th>
+                            </tr>
+                          </thead>
+                          <tbody id="listar_personal">
+
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- Tabla de clientes -->
+                <div class="tab-pane btn-cliente" id="cliente">
+                  <div class="card">
+                    <div class="card-header">Clientes</div>
+                    <div class="card-body table-responsive">
+                      <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                          <tr>
+                            <th>N°</th>
+                            <th>Nombres Cliente</th>
+                            <th>Apellidos</th>
+                            <th>DNI</th>
+                            <th>Telefono</th>
+                            <th>Correo Electronico</th>
+                            <th>Direccion</th>
+                            <th>Foto</th>
+                            <?php if ($_SESSION['rol_usuario'] == 1) {
+                            ?>
+                              <th>Editar</th>
+                              <th>Eliminar</th>
+                            <?php
+                            } ?>
+                          </tr>
+                        </thead>
+                        <tbody id="listar_cliente">
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <!-- /.container-fluid -->
+        </div>
+        <!-- contenedor de usuarios -->
       </section>
       <!-- /.content -->
     </div>
-
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark">
       <!-- Control sidebar content goes here -->
@@ -172,10 +233,10 @@ include_once "assets/views/nav.php";
 
 </body>
 
-    <script src="../vista/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-
-    <script src="../vista/assets/js/adminlte.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-
-    </div>
+<script src="../vista/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../vista/assets/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- J query -->
+</div>
